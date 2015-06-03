ef5b634a53b538874d9d28b6003a3908934b5759
Anis Da Silva Campos <anis@toto.fr>
2015-06-03 16:56:28 +0200

    MovePath Tour

--- game/src/model/Tour.java
+++ game/src/model/Tour.java
@@ -6,6 +6,8 @@
 package model;
 
 import static java.lang.Math.abs;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -19,10 +21,33 @@ public class Tour extends AbstractPiece {
 
     @Override
     public boolean isMoveOk(int xFinal, int yFinal) {
-        return estDansPlateau(xFinal, yFinal)&&((abs(xFinal - getX()) == 0
+        return estDansPlateau(xFinal, yFinal) && ((abs(xFinal - getX()) == 0
                 && abs(getY() - yFinal) > 0)
                 || (abs(xFinal - getX()) > 0
                 && abs(getY() - yFinal) == 0));
+    }
+
+    @Override
+    public List<Coord> movePath(int xFinal, int yFinal) {
+
+        int x = this.coord.x;
+        int y = this.coord.y;
+
+        List<Coord> ret = new ArrayList<>();
+
+        while (x != xFinal && y != yFinal) {
+            if (abs(xFinal - getX()) == 0 && (getY() - yFinal) > 0) {
+                y--;
+            } else if (abs(xFinal - getX()) == 0 && (getY() - yFinal) < 0) {
+                y++;
+            } else if ((xFinal - getX()) > 0 && abs(getY() - yFinal) == 0) {
+                x++;
+            } else if ((xFinal - getX()) < 0 && abs(getY() - yFinal) == 0) {
+                x--;
+            }
+        }
+
+        return ret;
     }
 
 }
